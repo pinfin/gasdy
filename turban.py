2cd8af102ba608dc05fadd8d0b7f24e0754cba7e
Anton Morozov <y.anton.morozov@yandex.ru>
2020-08-19 23:15:12 +0300

    добавлен расчёт КПД

--- turban.py
+++ turban.py
@@ -16,13 +16,16 @@ class PrelimDesign:
         self._rot = None
         self._pt = None
         self._tt = None
-        self._mu = None
-        self._R = None
-        self._k = None
+        self.mu = None
+        self.R = None
+        self.k = None
+        self.x = None
+        self.cp = None
         self._loss = None
         self._ratio_axial_c = None
         self._power = None
         self._pr_ts = None
+        self.es = None
 
         self.def_inlet_radius_ratio = 0.9
         self._d_in = None
@@ -33,6 +36,9 @@ class PrelimDesign:
         self._velocity_diagram = None
         self._max_stage = None
         self._min_stage = None
+
+        self.u_1 = None
+        self.u_n = None
 
     def set_stagnation_inlet_parameters(self, pt, tt, unit=SI):
 
@@ -54,13 +60,15 @@ class PrelimDesign:
         self._power = None
 
     def set_fluid(self, mu, R, k, unit=SI):
-        self._mu = mu * 1.48817 if unit == US else mu
-        self._R = R * 5.38 if unit == US else mu
-        self._k = k
+        self.mu = mu * 1.48817 if unit == US else mu
+        self.R = R * 5.38 if unit == US else mu
+        self.k = k
+        self.x = k / (k - 1)
+        self.cp = self.x * self.R
 
     def set_diameters(self, d_in, d_ex, unit):
-        self._d_in = d_in * 25.4 if unit == US else d_in
-        self._d_ex = d_ex * 25.4 if unit == US else d_ex
+        self._d_in = d_in * 25.4e-3 if unit == US else d_in
+        self._d_ex = d_ex * 25.4e-3 if unit == US else d_ex
 
     def set_mean_diameters(self, d_in, d_ex, units=SI):
         self.set_diameters(d_in, d_ex, units)
@@ -76,7 +84,7 @@ class PrelimDesign:
         self.set_diameters(d_in, d_ex, units)
 
     def set_exit_angle(self, alpha):
-        self._exit_angle = alpha
+        self._exit_angle = alpha * math.pi / 180
         self.exit_vane = False
 
     def set_exit_vane(self):
@@ -90,6 +98,109 @@ class PrelimDesign:
         self._max_stage = n_max
         self._min_stage = n_min
 
+    def get_dh_id(self):
+        return self.cp * self._tt * (1 - (1 / self._pr_ts) ** (1 / self.x))
+
+    def get_sum_sq_u(self, n_stage):
+        self.u_1 = self._rot * self._d_in
+        self.u_n = self._rot * self._d_ex
+        if n_stage == 1:
+            return self.u_n ** 2
+        else:
+            sum_sq_u = 0.0
+            for i in range(1, n_stage + 1):
+                u = (self.u_n - self.u_1) / (n_stage - 1) * (i - 1) + self.u_1
+                sum_sq_u += u ** 2
+            return sum_sq_u
+
+    def get_q_q2_fls(self, lam):
+        q = 0
+        q2 = 0
+        fls = 0
+        if self._velocity_diagram == SYMMETRY_VELOCITY_DIAGRAM:
+            q = (lam + 1) / 2
+            q2 = (lam - 1) / 2
+            fls = 2 - lam
+
+        if self._velocity_diagram == ZERO_EXIT_VELOCITY_DIAGRAM:
+            q = 1
+            q2 = 0
+            fls = 1
+
+        if self._velocity_diagram == IMPULSE_DIAGRAMS:
+            q = lam + 0.5
+            q2 = lam - 0.5
+            fls = 2 * (1 - lam) if lam < 0.5 else 1
+
+        if self._velocity_diagram == ZERO_EXIT_IMPULSE_VELOCITY_DIAGRAM:
+            q = 1 if lam >= 0.5 else lam + 0.5
+            q2 = 0 if lam >= 0.5 else lam - 0.5
+            fls = 1 if lam >= 0.5 else 2 * (1 - lam)
+        return q, q2, fls
+
+    def run(self):
+        self.es = 0.8
+        dh_id = self.get_dh_id()
+        p_ex = self._pt / self._pr_ts
+
+        del_ht = self.es * dh_id
+        tt_ex = self._tt - del_ht / self.cp
+
+        for n in range(self._min_stage, self._max_stage):
+            sum_sq_u = self.get_sum_sq_u(n)
+            lam = sum_sq_u / del_ht
+            q, q2, fls = self.get_q_q2_fls(lam)
+            dv_un = self.u_n / lam
+            vu_1n = q * dv_un
+            vu_2n = q2 * dv_un
+            cot = 1/math.tan(self._exit_angle)
+            c1 = (1 + 2 * cot ** 2) * q ** 2
+            ci = c1 + (q - 1) ** 2
+            d = 2 * cot ** 2 * q ** 2 + (q + lam) ** 2 + (q - lam - 1) ** 2
+
+            re = None
+            if n == 1:
+                re = self._mf / self.mu / self._d_ex * 2
+            else:
+                re = self._mf / self.mu / self._d_in * 2
+
+            a = self._loss / cot / re ** 0.2
+            a1 = a * (c1 + 2 * d)
+            ai = a * (fls * ci + 2 * d)
+            b = self._ratio_axial_c * cot ** 2 * q ** 2 + q2 ** 2
+            et1 = lam / (lam + a1/2)
+            es1 = lam / (lam + (a1 + b)/2)
+            eti = lam / (lam + ai / 2)
+            esi = lam / (lam + (ai + b) / 2)
+
+            if self.exit_vane:
+                cl = 2 * cot ** 2 * q ** 2 + q2 ** 2
+                fls_l = 1
+                a1l = a1 + a * cl * fls_l
+                ail = ai + a * cl * fls_l
+                bl = self._ratio_axial_c * cot ** 2 * q ** 2
+                et1_l = lam / (lam + a1l / 2)
+                es1_l = lam / (lam + (a1l + bl) / 2)
+                eti_l = lam / (lam + ail / 2)
+                esi_l = lam / (lam + (ail + bl) / 2)
+                if n == 1:
+                    self.es = es1_l
+                else:
+                    self.es = 1 / (self.u_1 ** 2 / et1 / sum_sq_u + (1 - (self.u_1 ** 2 + self.u_n ** 2) / sum_sq_u) /
+                                   eti_l + self.u_n ** 2 / esi_l / sum_sq_u)
+            else:
+                if n == 1:
+                    self.es = es1
+                else:
+                    self.es = 1 / (self.u_1 ** 2 / et1 / sum_sq_u +
+                                   (1 - (self.u_1 ** 2 + self.u_n ** 2) / sum_sq_u) / eti_l) + \
+                              self.u_n ** 2 / esi / sum_sq_u
+
+
+
+
+
+
 
 turbine = PrelimDesign()
 turbine.set_stagnation_inlet_parameters(113.1, 2660, US)
@@ -101,5 +212,5 @@ turbine.set_exit_angle(65)
 turbine.set_number_stage(1, 2)
 turbine.set_velocity_diagram(SYMMETRY_VELOCITY_DIAGRAM)
 
-#TODO: дописать
+
 
